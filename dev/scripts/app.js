79c457192734726cf1245e0e535a0ec87d9b3415
Gage Wagner <gageplwagner@gmail.com>
2017-11-28 16:58:58 -0500

    Added router and middleware

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 class App extends React.Component {
     render() {
