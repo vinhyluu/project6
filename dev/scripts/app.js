8fd4356f81b97f895ab1e810cfe0eb5b8ea85e96
Gage Wagner <gageplwagner@gmail.com>
2017-11-29 17:32:16 -0500

    ok

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-<<<<<<< HEAD
 import firebase from 'firebase';
-=======
-import SearchForm from './SearchForm'
->>>>>>> dbb028cdee5bf8394b5214eccc5a4fea39f8791f
+import SearchForm from './SearchForm';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Landing from './landing';
 
@@ -23,12 +20,9 @@ class App extends React.Component {
     render() {
       return (
         <div>
-<<<<<<< HEAD
           <Landing />
-=======
           <h1>Makeup App</h1>
           <SearchForm />
->>>>>>> dbb028cdee5bf8394b5214eccc5a4fea39f8791f
         </div>
       )
     }
