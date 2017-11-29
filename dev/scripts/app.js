4d6757a008b910abca8284d59d80598238bc310c
Chelsea Roman <chelsea.ann.roman@gmail.com>
2017-11-29 11:01:42 -0500

    i dont get git

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
+
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <Heading/>
+      </div>
+    )
+  }
 }
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
