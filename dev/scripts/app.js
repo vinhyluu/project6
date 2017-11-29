6fdc70dbfb1265966efcdc5b48187d1e008eb747
Gage Wagner <gageplwagner@gmail.com>
2017-11-29 16:17:37 -0500

    beta auth

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import firebase from 'firebase';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Landing from './landing';
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          <Landing />
         </div>
       )
     }
