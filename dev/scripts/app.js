665e482d87392f161158b3aa1ae3017d66eead55
Eugenia Lau <eugenia.ch.lau@gmail.com>
2017-11-29 16:22:36 -0500

    put everything into profile

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import firebase from 'firebase';
+import Profile from './profile';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import Login from './login';
+
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
+  
+  render() {
+    return (
+      <div>
+        <Profile />
+      </div>
+    )
+  }
 }
-
 ReactDOM.render(<App />, document.getElementById('app'));
