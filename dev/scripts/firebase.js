701d72fcf8f16bffacafe63ea63590b40e019272
Eugenia Lau <eugenia.ch.lau@gmail.com>
2017-12-01 12:04:40 -0500

    need to merge

--- dev/scripts/firebase.js
+++ dev/scripts/firebase.js
@@ -8,6 +8,15 @@ const config = {
     storageBucket: "makeup-fun.appspot.com",
     messagingSenderId: "1012365554610"
 };
+
+// var config = {
+//   apiKey: "AIzaSyCj6H_1C3GR8cp2D3p4qTTMakeLJMDMaw0",
+//   authDomain: "my-awesome-project-6.firebaseapp.com",
+//   databaseURL: "https://my-awesome-project-6.firebaseio.com",
+//   projectId: "my-awesome-project-6",
+//   storageBucket: "my-awesome-project-6.appspot.com",
+//   messagingSenderId: "682386649526"
+// };
 firebase.initializeApp(config); 
 
 export default firebase;
