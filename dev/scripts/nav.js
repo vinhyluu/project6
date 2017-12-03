ccb0ac7ec5e0fbf04ad4e2137ac7910458ec3204
Gage Wagner <gageplwagner@gmail.com>
2017-12-03 16:28:10 -0500

    started styles

--- dev/scripts/nav.js
+++ dev/scripts/nav.js
@@ -1,37 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import AdminView from './AdminView'; 
-import SearchForm from './SearchForm'; 
-import PublicPage from './PublicPage'; 
-import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom';
-
-class Nav extends React.Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <header>
-                        <nav>
-                            <NavLink to="/adminview" activeClassName="active">
-                                <i className="fa fa-user-o" aria-hidden="true"></i>
-                            </NavLink>
-                            <NavLink to="/searchform" activeClassName="active">
-                                <i className="fa fa-search" aria-hidden="true"></i>
-                            </NavLink>
-                            <NavLink to="/publicpage" activeClassName="active">
-                                <i className="fa fa-share-alt" aria-hidden="true"></i>
-                            </NavLink>
-                        </nav>
-                    </header>
-                    <Switch>
-                        <Route exactly component={AdminView} pattern="/adminview" />
-                        <Route exactly component={PublicPage} pattern="/publicpage" />
-                        <Route exactly component={SearchForm} pattern="/searchform" />
-                    </Switch>
-                </div>
-            </Router>
-=======
 
 class Nav extends React.Component {
     render(){
@@ -45,7 +13,6 @@ class Nav extends React.Component {
                     </nav>
                 </header>
             </div>            
->>>>>>> 125f7583f90cca5a403f7e46c403565cc5683ba7
         )
     }
 }
