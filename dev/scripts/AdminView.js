96e03f4f177386daa529eef3bc44c05be4274cb3
Chelsea Roman <chelsea.ann.roman@gmail.com>
2017-12-03 16:26:32 -0500

    merging issue for editing box

--- dev/scripts/AdminView.js
+++ dev/scripts/AdminView.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import firebase from './firebase';
 import EditingBox from './EditingBox';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
 class AdminView extends React.Component{
     constructor(){
         super();
@@ -11,22 +10,17 @@ class AdminView extends React.Component{
             currentItems: [],
             active: false,
             testColor: "",
-
         }
         this.removeItem = this.removeItem.bind(this);
         this.toggleClass = this.toggleClass.bind(this);
-        this.toggleColor = this.toggleColor.bind(this);
         this.addPublic = this.addPublic.bind(this);
         this.removePublic = this.removePublic.bind(this);
     }
-
     componentDidMount(){
         const dbRef = firebase.database().ref("N5eadjZta9gfwlPBYiKIx2Q1G7v1").child("selections");
-
         const userItems = [];
         dbRef.once("value", (res) => {
             const data = res.val();
-
             for (let key in data) {
                 const value = data[key];
                 userItems.push(value);
@@ -37,53 +31,41 @@ class AdminView extends React.Component{
         })
     }
     
-    
     removeItem(e, key) {
         e.preventDefault();
-        const selectionRef = key;
         const toRemove = firebase.database().ref("N5eadjZta9gfwlPBYiKIx2Q1G7v1").child(`selections/${key}`);
-        
         toRemove.remove();
-
         const dbRef = firebase.database().ref("N5eadjZta9gfwlPBYiKIx2Q1G7v1").child("selections");
-
-        const userItems = [];
+        const newUserItems = [];
         dbRef.on("value", (res) => {
             const data = res.val();
-
             for (let key in data) {
                 const value = data[key];
-                userItems.push(value);
+                newUserItems.push(value);
             }
             this.setState({
-                currentItems: userItems,
-                
+                currentItems: newUserItems
             })
         })
     }
-
-    toggleClass(e){
-        e.preventDefault();
     addPublic(e, key) {
         e.preventDefault();
         const dbRef = firebase.database().ref("N5eadjZta9gfwlPBYiKIx2Q1G7v1").child(`selections/${key}`);
-
         dbRef.update({
             active: true,
         });
     }
-
     removePublic(e, key) {
         e.preventDefault();
         const dbRef = firebase.database().ref("N5eadjZta9gfwlPBYiKIx2Q1G7v1").child(`selections/${key}`);
-
         dbRef.update({
             active: false,
         });
     }
-
-        //store null or active value for each item added to shareable list on adminpage before we decide to add to shareable list
-
+    toggleClass(){
+        const currentState = this.state.active;
+        this.setState({ active: !currentState})
+    }
     toggleColor(e, colorValue){
         e.preventDefault();
         this.setState({
@@ -102,78 +84,45 @@ class AdminView extends React.Component{
             width: "50px",
             height: "50px",
         }
-
-
-        return(
+        return (
             <div>
                 <div>
                     <div>
-                        <div>
-                            <img src={`${this.state.imageUrl}`} alt=""/>
-                            
-
-
-                            <EditingBox/>
-                            
-                            <p>{this.state.note}</p>
-                            <a href={`${this.state.twitter}`}>
-                                <i className="fa fa-twitter" aria-hidden="true"></i>
-                            </a>
-                            <a href={`${this.state.instagram}`}>
-                                <i className="fa fa-instagram" aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    
-
-                    <p>{this.state.note}</p>
-                    <a href={`${this.state.twitter}`}>
-                        <i className="fa fa-twitter" aria-hidden="true"></i>
-                    </a>
-                    <a href={`${this.state.instagram}`}>
-                        <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </a>
-                
-                    {this.state.currentItems.map((item)=>{
-                        return(
-                        <div key={item.selectionKey}>
-                            <div className="wrapper">
-                                <img src={item.imageUrl} alt=""/> 
-                                <h1>{item.brandTitle}</h1>
-                                <p>{item.productDescription}</p>
-                                <a href="" onClick={(e) => this.removeItem(e, item.selectionKey)}>test</a>
-                                <a href="" onClick={(e) => this.addPublic(e, item.selectionKey)}>Add Public</a>
-                                <a href="" onClick={(e) => this.removePublic(e, item.selectionKey)}>Remove Public</a>
+                      
+                        <EditingBox />
+                        <p>{this.state.note}</p>
+                        <a href={`${this.state.twitter}`}>
+                            <i className="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                        <a href={`${this.state.instagram}`}>
+                            <i className="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                    {this.state.currentItems.map((item) => {
+                        return (
+                            <div key={item.selectionKey}>
+                                <div className="wrapper">
+                                    <img src={item.imageUrl} alt="" />
+                                    <h1>{item.brandTitle}</h1>
+                                    <p>{item.productDescription}</p>
+                                    <a href="" onClick={(e) => this.removeItem(e, item.selectionKey)}>test</a>
+                                    <a href="" onClick={(e) => this.addPublic(e, item.selectionKey)}>Add Public</a>
+                                    <a href="" onClick={(e) => this.removePublic(e, item.selectionKey)}>Remove Public</a>
+                                </div>
                             </div>
-                        </div>
                         )
                     })}
                 </div>
-                
-                {this.state.currentItems.map((item)=>{
-                    return(
-                    <div key={item.selectionKey}>
-                        <div className="wrapper">
-                            <img src={item.imageUrl} alt=""/> 
-                            <h1>{item.brandTitle}</h1>
-                            <p>{item.productDescription}</p>
-                            <a href="" onClick={(e) => this.removeItem(e, item.selectionKey)}>test</a>
-                            <a href="" onClick={(e) => this.addPublic(e, item.selectionKey)}>Add Public</a>
-                            <a href="" onClick={(e) => this.removePublic(e, item.selectionKey)}>Remove Public</a>
-                        </div>
-                    </div>
-                    )
-                })}
-            {/* <Link to={}>
-            </Link> */}
-            <div>
-                <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption1")}></div>
-                <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption2")}></div>
-                <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption3")}></div>
-                <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption4")}></div>
-        </div>       
-    </div>    
+                {/* <Link>
+                </Link> */}
+                <div>
+                    <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption1")}></div>
+                    <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption2")}></div>
+                    <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption3")}></div>
+                    <div style={divStyle} onClick={(e) => this.toggleColor(e, "userOption4")}></div>
+                </div>
+            </div>
         )
     }
 }
-
 export default AdminView
